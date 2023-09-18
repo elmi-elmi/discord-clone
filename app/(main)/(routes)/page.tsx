8cc16e449e65c556957aa689ae04f6e2b9db108e
elmi.a <shahrokhelmi@gmail.com>
2023-09-18 12:11:26 +0330

    auto login with oauth2

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,15 @@
+import { UserButton } from "@clerk/nextjs";
 
 const state = false;
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-semibold text-blue-400">Home Page: Protected Route</h1>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      <h1 className="text-3xl font-semibold text-blue-400">
+        Home Page: Protected Route
+      </h1>
     </>
-    
-  )
+  );
 }
